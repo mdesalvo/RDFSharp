2a9b56a0ad6532e7df530ce4376e7e39def8a3bd
Marco De Salvo <mdesalvo@users.noreply.github.com>
2018-05-20 12:16:27 +0200

    Added info/warning events

---
+++ RDFSharp/Store/RDFStoreEvents.cs
@@ -0,0 +1,68 @@
+﻿/*
+   Copyright 2012-2018 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Threading.Tasks;
+
+namespace RDFSharp.Store
+{
+
+    /// <summary>
+    /// RDFStoreEvents represents a collector for all the events generated within the "RDFSharp.Store" namespace
+    /// </summary>
+    public static class RDFStoreEvents {
+
+        #region OnStoreInfo
+        /// <summary>
+        /// Event representing an information message generated within the "RDFSharp.Store" namespace
+        /// </summary>
+        public static event RDFStoreInfoEventHandler OnStoreInfo = delegate { };
+
+        /// <summary>
+        /// Delegate to handle information events generated within the "RDFSharp.Store" namespace
+        /// </summary>
+        public delegate void RDFStoreInfoEventHandler(String eventMessage);
+
+        /// <summary>
+        /// Internal invoker of the subscribed information event handler
+        /// </summary>
+        internal static void RaiseStoreInfo(String eventMessage) {
+            Parallel.Invoke(() => OnStoreInfo(DateTime.Now.ToString() + ";STORE_INFO;" + eventMessage));
+        }
+        #endregion
+
+        #region OnStoreWarning
+        /// <summary>
+        /// Event representing a warning message generated within the "RDFSharp.Store" namespace
+        /// </summary>
+        public static event RDFStoreWarningEventHandler OnStoreWarning = delegate { };
+
+        /// <summary>
+        /// Delegate to handle warning events generated within the "RDFSharp.Store" namespace
+        /// </summary>
+        public delegate void RDFStoreWarningEventHandler(String eventMessage);
+
+        /// <summary>
+        /// Internal invoker of the subscribed warning event handler
+        /// </summary>
+        internal static void RaiseStoreWarning(String eventMessage) {
+            Parallel.Invoke(() => OnStoreWarning(DateTime.Now.ToString() + ";STORE_WARNING;" + eventMessage));
+        }
+        #endregion
+
+    }
+
+}
