c8482588484991c9316ef6a0b385deca3f71bb12
mdesalvo <mdesalvo@localhost>
2019-05-21 20:27:15 +0200

    #61 Added support for IN filter

---
+++ RDFSharp/Query/Mirella/Algebra/Filters/RDFInFilter.cs
@@ -0,0 +1,110 @@
+﻿/*
+   Copyright 2012-2019 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using RDFSharp.Model;
+using RDFSharp.Store;
+
+namespace RDFSharp.Query
+{
+
+    /// <summary>
+    /// RDFInFilter represents a filter checking if a variable is found in a given set of RDF terms.
+    /// </summary>
+    public class RDFInFilter : RDFFilter
+    {
+
+        #region Properties
+        /// <summary>
+        /// RDF term to be searched
+        /// </summary>
+        public RDFPatternMember TermToSearch { get; internal set; }
+
+        /// <summary>
+        /// List of RDF terms in which the term should be searched
+        /// </summary>
+        internal List<RDFPatternMember> InTerms { get; set; }
+        #endregion
+
+        #region Ctors
+        /// <summary>
+        /// Default-ctor to build a filter on the given term and given search list
+        /// </summary>
+        public RDFInFilter(RDFPatternMember termToSearch, List<RDFPatternMember> inTerms)
+        {
+            if (termToSearch != null)
+            {
+                this.TermToSearch = termToSearch;
+                this.InTerms = inTerms ?? new List<RDFPatternMember>();
+            }
+            else
+            {
+                throw new RDFQueryException("Cannot create RDFInFilter because given \"termToSearch\" parameter is null.");
+            }
+        }
+        #endregion
+
+        #region Interfaces
+        /// <summary>
+        /// Gives the string representation of the filter 
+        /// </summary>
+        public override String ToString()
+        {
+            return this.ToString(new List<RDFNamespace>());
+        }
+        internal override String ToString(List<RDFNamespace> prefixes)
+        {
+            return $"FILTER ( {RDFQueryPrinter.PrintPatternMember(this.TermToSearch, prefixes)} IN ({String.Join(", ", this.InTerms.Select(t => RDFQueryPrinter.PrintPatternMember(t, prefixes)))}) )";
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Applies the filter on the column corresponding to the variable in the given datarow 
+        /// </summary>
+        internal override Boolean ApplyFilter(DataRow row, Boolean applyNegation)
+        {
+            Boolean keepRow = false;
+
+            //IN filter is equivalent to an OR-chain of equality comparison filters
+            IEnumerator<RDFPatternMember> inTermsEnumerator = this.InTerms.GetEnumerator();
+            while(inTermsEnumerator.MoveNext())
+            {
+                RDFComparisonFilter compFilter = new RDFComparisonFilter(RDFQueryEnums.RDFComparisonFlavors.EqualTo, this.TermToSearch, inTermsEnumerator.Current);
+                keepRow = compFilter.ApplyFilter(row, applyNegation);
+                if (keepRow)
+                {
+                    break;
+                }
+            }
+
+            //Apply the eventual negation
+            if (applyNegation)
+            {
+                keepRow = !keepRow;
+            }
+
+            return keepRow;
+        }
+        #endregion
+
+    }
+
+}
