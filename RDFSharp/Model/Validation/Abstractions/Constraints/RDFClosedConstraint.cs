7401f4edb6a8d0d1606682abaaa32c1013bb2c8f
mdesalvo <mdesalvo@localhost>
2020-04-18 11:38:00 +0200

    Corrected typo in ctor comment

--- RDFSharp/Model/Validation/Abstractions/Constraints/RDFClosedConstraint.cs
+++ RDFSharp/Model/Validation/Abstractions/Constraints/RDFClosedConstraint.cs
@@ -39,7 +39,7 @@ namespace RDFSharp.Model
 
         #region Ctors
         /// <summary>
-        /// Default-ctor to build a in constraint of the given type (Resource/Literal)
+        /// Default-ctor to build a closed constraint with the given behavior
         /// </summary>
         public RDFClosedConstraint(Boolean closed) : base() {
             this.Closed = closed;
