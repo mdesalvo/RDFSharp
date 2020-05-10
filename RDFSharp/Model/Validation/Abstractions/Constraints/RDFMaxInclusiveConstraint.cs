abf9bba2dcedff87449294161e017ff330a1f910
mdesalvo <mdesalvo@localhost>
2020-05-10 10:48:03 +0200

    WIP refactoring of SHACL engine

--- RDFSharp/Model/Validation/Abstractions/Constraints/RDFMaxInclusiveConstraint.cs
+++ RDFSharp/Model/Validation/Abstractions/Constraints/RDFMaxInclusiveConstraint.cs
@@ -16,6 +16,7 @@
 
 using RDFSharp.Query;
 using System;
+using System.Collections.Generic;
 
 namespace RDFSharp.Model
 {
@@ -61,31 +62,22 @@ namespace RDFSharp.Model
         /// <summary>
         /// Evaluates this constraint against the given data graph
         /// </summary>
-        internal override RDFValidationReport Evaluate(RDFValidationContext validationContext) {
-            RDFValidationReport report = new RDFValidationReport(new RDFResource());
+        internal override RDFValidationReport ValidateConstraint(RDFShapesGraph shapesGraph, RDFGraph dataGraph, RDFShape shape, RDFPatternMember focusNode, List<RDFPatternMember> valueNodes) {
+            RDFValidationReport report = new RDFValidationReport();
 
             #region Evaluation
-            //Evaluate focus nodes
-            validationContext.FocusNodes.ForEach(focusNode => {
-
-                //Get value nodes of current focus node
-                validationContext.ValueNodes[focusNode.PatternMemberID].ForEach(valueNode => {
-
-                    //Evaluate current value node
-                    Int32 comparison = RDFQueryUtilities.CompareRDFPatternMembers(this.Value, valueNode);
-                    if (comparison == -99 || comparison < 0) {
-                        report.AddResult(new RDFValidationResult(validationContext.Shape,
-                                                                 RDFVocabulary.SHACL.MAX_INCLUSIVE_CONSTRAINT_COMPONENT,
-                                                                 focusNode,
-                                                                 validationContext.Shape is RDFPropertyShape ? ((RDFPropertyShape)validationContext.Shape).Path : null,
-                                                                 valueNode,
-                                                                 validationContext.Shape.Messages,
-                                                                 validationContext.Shape.Severity));
-                    }
-
-                });
-
-            });
+            foreach (RDFPatternMember valueNode in valueNodes) {
+                Int32 comparison = RDFQueryUtilities.CompareRDFPatternMembers(this.Value, valueNode);
+                if (comparison == -99 || comparison < 0) {
+                    report.AddResult(new RDFValidationResult(shape,
+                                                             RDFVocabulary.SHACL.MAX_INCLUSIVE_CONSTRAINT_COMPONENT,
+                                                             focusNode,
+                                                             shape is RDFPropertyShape ? ((RDFPropertyShape)shape).Path : null,
+                                                             valueNode,
+                                                             shape.Messages,
+                                                             shape.Severity));
+                }
+            }
             #endregion
 
             return report;
