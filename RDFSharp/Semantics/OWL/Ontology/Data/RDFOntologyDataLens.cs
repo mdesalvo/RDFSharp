0581234731b559766d12be46ba55bbb97c6e1573
Marco De Salvo <mdesalvo@users.noreply.github.com>
2021-02-11 16:03:24 +0100

    Update RDFOntologyDataLens.cs

--- RDFSharp/Semantics/OWL/Ontology/Data/RDFOntologyDataLens.cs
+++ RDFSharp/Semantics/OWL/Ontology/Data/RDFOntologyDataLens.cs
@@ -67,15 +67,6 @@ namespace RDFSharp.Semantics.OWL
 
         #region Methods
         /// <summary>
-        /// Initializes the lens ontology with support for SKOS ontology
-        /// </summary>
-        public RDFOntologyDataLens InitializeSKOS()
-        {
-            this.Ontology = this.Ontology.UnionWith(RDFSKOSOntology.Instance);
-            return this;
-        }
-
-        /// <summary>
         /// Enlists the facts which are directly (or indirectly, if inference is requested) equivalent to the lens fact
         /// </summary>
         public List<(bool, RDFOntologyFact)> SameFacts(bool enableInference)
