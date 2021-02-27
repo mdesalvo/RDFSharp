9d140ac1dabe9cc999fdad499bb67bb32e556d29
mdesalvo <mdesalvo@localhost>
2021-02-27 23:52:27 +0100

    Added OWL2 bottom properties to BASE ontology

--- RDFSharp/Semantics/OWL/Ontology/RDFOntology.cs
+++ RDFSharp/Semantics/OWL/Ontology/RDFOntology.cs
@@ -790,6 +790,8 @@ namespace RDFSharp.Semantics.OWL
             Instance.Model.PropertyModel.AddProperty(RDFVocabulary.OWL.PROPERTY_CHAIN_AXIOM.ToRDFOntologyObjectProperty());
             Instance.Model.PropertyModel.AddProperty(RDFVocabulary.OWL.TOP_OBJECT_PROPERTY.ToRDFOntologyObjectProperty());
             Instance.Model.PropertyModel.AddProperty(RDFVocabulary.OWL.TOP_DATA_PROPERTY.ToRDFOntologyDatatypeProperty());
+            Instance.Model.PropertyModel.AddProperty(RDFVocabulary.OWL.BOTTOM_OBJECT_PROPERTY.ToRDFOntologyObjectProperty());
+            Instance.Model.PropertyModel.AddProperty(RDFVocabulary.OWL.BOTTOM_DATA_PROPERTY.ToRDFOntologyDatatypeProperty());
 
             #endregion
 
