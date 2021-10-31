2839ff2d68846eaa3a06ddf031947e4ea335b894
Marco De Salvo <floating_point_9@msn.com>
2021-10-31 19:00:54 +0100

    Added tests for RDFDataSource

---
+++ RDFSharp.Test/Model/RDFDatasourceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RDFSharp.Model;
+using RDFSharp.Query;
+using RDFSharp.Store;
+using System;
+
+namespace RDFSharp.Test.Model
+{
+    [TestClass]
+    public class RDFDataSourceTest
+    {
+        [TestMethod]
+        public void ShouldCreateDataSourceOfTypeGraph()
+            => Assert.IsTrue(new RDFGraph().IsGraph());
+
+        [TestMethod]
+        public void ShouldCreateDataSourceOfTypeStore()
+            => Assert.IsTrue(new RDFMemoryStore().IsStore());
+
+        [TestMethod]
+        public void ShouldCreateDataSourceOfTypeFederation()
+            => Assert.IsTrue(new RDFFederation().IsFederation());
+
+        [TestMethod]
+        public void ShouldCreateDataSourceOfTypeSPARQLEndpoint()
+            => Assert.IsTrue(new RDFSPARQLEndpoint(new Uri("http://example.org/")).IsSPARQLEndpoint());
+    }
+}
