0821c1b15c9d9ff08757412403ab1345dbe7f73c
mdesalvo <floating_point_9@msn.com>
2022-01-01 17:48:05 +0100

    Added tests for SHACL LessThanOrEquals constraint

--- RDFSharp/Model/Validation/Abstractions/Constraints/RDFLessThanOrEqualsConstraint.cs
+++ RDFSharp/Model/Validation/Abstractions/Constraints/RDFLessThanOrEqualsConstraint.cs
@@ -58,6 +58,11 @@ namespace RDFSharp.Model
         {
             RDFValidationReport report = new RDFValidationReport();
 
+            //In case no shape messages have been provided, this constraint emits a default one (for usability)
+            List<RDFLiteral> shapeMessages = new List<RDFLiteral>(shape.Messages);
+            if (shapeMessages.Count == 0)
+                shapeMessages.Add(new RDFPlainLiteral($"Must have values less than or equals to <{this.LessThanOrEqualsPredicate}>"));
+
             #region Evaluation
             List<RDFPatternMember> predicateNodes = dataGraph.Where(t => t.Subject.Equals(focusNode)
                                                                             && t.Predicate.Equals(this.LessThanOrEqualsPredicate))
@@ -75,7 +80,7 @@ namespace RDFSharp.Model
                                                                  focusNode,
                                                                  shape is RDFPropertyShape ? ((RDFPropertyShape)shape).Path : null,
                                                                  valueNode,
-                                                                 shape.Messages,
+                                                                 shapeMessages,
                                                                  shape.Severity));
                 }
             }
