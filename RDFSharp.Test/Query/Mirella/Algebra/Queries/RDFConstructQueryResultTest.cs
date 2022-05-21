16a38d027a6337190de3de8ea81d3c03f2e56f0b
mdesalvo <floating_point_9@msn.com>
2022-05-21 19:55:04 +0200

    WIP continue tests on RDFConstructQueryResult

--- RDFSharp.Test/Query/Mirella/Algebra/Queries/RDFConstructQueryResultTest.cs
+++ RDFSharp.Test/Query/Mirella/Algebra/Queries/RDFConstructQueryResultTest.cs
@@ -82,6 +82,48 @@ namespace RDFSharp.Test.Query
             Assert.IsTrue(constructGraph.ContainsTriple(new RDFTriple(new RDFResource("ex:subj"),new RDFResource("ex:pred"),new RDFPlainLiteral("lit"))));
             Assert.IsTrue(constructGraph.ContainsTriple(new RDFTriple(new RDFResource("ex:subj"),new RDFResource("ex:pred"),new RDFTypedLiteral("lit", RDFModelEnums.RDFDatatypes.XSD_STRING))));
         }
+
+        [TestMethod]
+        public async Task ShouldSerializeConstructQueryResultToGraphAsync()
+        {
+            RDFConstructQueryResult constructResult = new RDFConstructQueryResult();
+            constructResult.ConstructResults.Columns.Add(new DataColumn("?SUBJECT", typeof(string)));
+            constructResult.ConstructResults.Columns.Add(new DataColumn("?PREDICATE", typeof(string)));
+            constructResult.ConstructResults.Columns.Add(new DataColumn("?OBJECT", typeof(string)));
+            DataRow row0 = constructResult.ConstructResults.NewRow();
+            row0["?SUBJECT"] = "ex:subj";
+            row0["?PREDICATE"] = "ex:pred";
+            row0["?OBJECT"] = "lit@EN-US";
+            constructResult.ConstructResults.Rows.Add(row0);
+            DataRow row1 = constructResult.ConstructResults.NewRow();
+            row1["?SUBJECT"] = "bnode:12345";
+            row1["?PREDICATE"] = $"{RDFVocabulary.RDF.TYPE}";
+            row1["?OBJECT"] = $"ex:obj";
+            constructResult.ConstructResults.Rows.Add(row1);
+            DataRow row2 = constructResult.ConstructResults.NewRow();
+            row2["?SUBJECT"] = "ex:subj";
+            row2["?PREDICATE"] = "ex:pred";
+            row2["?OBJECT"] = "lit";
+            constructResult.ConstructResults.Rows.Add(row2);
+            DataRow row3 = constructResult.ConstructResults.NewRow();
+            row3["?SUBJECT"] = "ex:subj";
+            row3["?PREDICATE"] = "ex:pred";
+            row3["?OBJECT"] = $"lit^^{RDFVocabulary.XSD.STRING}";
+            constructResult.ConstructResults.Rows.Add(row3);
+            constructResult.ConstructResults.AcceptChanges();
+            
+            Assert.IsTrue(constructResult.ConstructResults.Columns.Count == 3);
+            Assert.IsTrue(constructResult.ConstructResultsCount == 4);
+
+            RDFGraph constructGraph = await constructResult.ToRDFGraphAsync();
+            
+            Assert.IsNotNull(constructGraph);
+            Assert.IsTrue(constructGraph.TriplesCount == 4);
+            Assert.IsTrue(constructGraph.ContainsTriple(new RDFTriple(new RDFResource("ex:subj"),new RDFResource("ex:pred"),new RDFPlainLiteral("lit","en-US"))));
+            Assert.IsTrue(constructGraph.ContainsTriple(new RDFTriple(new RDFResource("bnode:12345"),RDFVocabulary.RDF.TYPE,new RDFResource("ex:obj"))));
+            Assert.IsTrue(constructGraph.ContainsTriple(new RDFTriple(new RDFResource("ex:subj"),new RDFResource("ex:pred"),new RDFPlainLiteral("lit"))));
+            Assert.IsTrue(constructGraph.ContainsTriple(new RDFTriple(new RDFResource("ex:subj"),new RDFResource("ex:pred"),new RDFTypedLiteral("lit", RDFModelEnums.RDFDatatypes.XSD_STRING))));
+        }
         #endregion
     }
 }
