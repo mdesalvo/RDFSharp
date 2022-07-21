1bc5b3b3bc02abe3e819e15eda2bc846d1dfcafe
mdesalvo <floating_point_9@msn.com>
2022-07-21 20:04:10 +0200

    Add more tests for taxonomy

--- RDFSharp.Test/Semantics/OWL/Ontology/RDFOntologyTaxonomyTest.cs
+++ RDFSharp.Test/Semantics/OWL/Ontology/RDFOntologyTaxonomyTest.cs
@@ -15,6 +15,7 @@
 */
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RDFSharp.Model;
 using RDFSharp.Semantics.OWL;
 using System.Collections.Generic;
 
@@ -48,6 +49,89 @@ namespace RDFSharp.Test.Semantics
             IEnumerator<RDFOntologyTaxonomyEntry> taxonomyEntriesEnumerator = taxonomy.EntriesEnumerator;
             while (taxonomyEntriesEnumerator.MoveNext()) j++;
             Assert.IsTrue(j == 0);
+        }
+
+        [TestMethod]
+        public void ShouldAddEntryNotAcceptingDuplicates()
+        {
+            RDFOntologyTaxonomy taxonomy = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Model, false);
+            RDFOntologyTaxonomyEntry taxonomyEntry = new RDFOntologyTaxonomyEntry(new RDFOntologyClass(new RDFResource("ex:class")),new RDFOntologyProperty(new RDFResource("ex:predicate")),new RDFOntologyLiteral(new RDFPlainLiteral("lit")));
+            
+            Assert.IsTrue(taxonomy.AddEntry(taxonomyEntry));
+            Assert.IsFalse(taxonomy.AddEntry(taxonomyEntry)); //Will be discarded, since duplicate taxonomy entries are not allowed (in this test)
+            Assert.IsFalse(taxonomy.AddEntry(null)); //Will be discarded, since null taxonomy entries are not allowed
+            Assert.IsTrue(taxonomy.EntriesCount == 1);
+            Assert.IsTrue(taxonomy.EntriesLookup.Count == 1);
+
+            int i = 0;
+            foreach (RDFOntologyTaxonomyEntry te in taxonomy) i++;
+            Assert.IsTrue(i == 1);
+
+            int j = 0;
+            IEnumerator<RDFOntologyTaxonomyEntry> taxonomyEntriesEnumerator = taxonomy.EntriesEnumerator;
+            while (taxonomyEntriesEnumerator.MoveNext()) j++;
+            Assert.IsTrue(j == 1);
+        }
+
+        [TestMethod]
+        public void ShouldAddEntryAcceptingDuplicates()
+        {
+            RDFOntologyTaxonomy taxonomy = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Model, true);
+            RDFOntologyTaxonomyEntry taxonomyEntry = new RDFOntologyTaxonomyEntry(new RDFOntologyClass(new RDFResource("ex:class")),new RDFOntologyProperty(new RDFResource("ex:predicate")),new RDFOntologyLiteral(new RDFPlainLiteral("lit")));
+            
+            Assert.IsTrue(taxonomy.AddEntry(taxonomyEntry));
+            Assert.IsTrue(taxonomy.AddEntry(taxonomyEntry));
+            Assert.IsFalse(taxonomy.AddEntry(null)); //Will be discarded, since null taxonomy entries are not allowed
+            Assert.IsTrue(taxonomy.EntriesCount == 2);
+            Assert.IsTrue(taxonomy.EntriesLookup.Count == 1);
+
+            int i = 0;
+            foreach (RDFOntologyTaxonomyEntry te in taxonomy) i++;
+            Assert.IsTrue(i == 2);
+
+            int j = 0;
+            IEnumerator<RDFOntologyTaxonomyEntry> taxonomyEntriesEnumerator = taxonomy.EntriesEnumerator;
+            while (taxonomyEntriesEnumerator.MoveNext()) j++;
+            Assert.IsTrue(j == 2);
+        }
+
+        [TestMethod]
+        public void ShouldRemoveEntry()
+        {
+            RDFOntologyTaxonomy taxonomy = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Model, true);
+            RDFOntologyTaxonomyEntry taxonomyEntry = new RDFOntologyTaxonomyEntry(new RDFOntologyClass(new RDFResource("ex:class")),new RDFOntologyProperty(new RDFResource("ex:predicate")),new RDFOntologyLiteral(new RDFPlainLiteral("lit")));
+            RDFOntologyTaxonomyEntry taxonomyEntry2 = new RDFOntologyTaxonomyEntry(new RDFOntologyClass(new RDFResource("ex:class")),new RDFOntologyProperty(new RDFResource("ex:predicate")),new RDFOntologyLiteral(new RDFPlainLiteral("lit2")));
+            taxonomy.AddEntry(taxonomyEntry);
+            taxonomy.AddEntry(taxonomyEntry);
+
+            Assert.IsTrue(taxonomy.RemoveEntry(taxonomyEntry));
+            Assert.IsFalse(taxonomy.RemoveEntry(taxonomyEntry));
+            Assert.IsFalse(taxonomy.RemoveEntry(taxonomyEntry2)); //This entry was not added to the taxonomy
+            Assert.IsFalse(taxonomy.RemoveEntry(null)); //Will be discarded, since null taxonomy entries are not allowed
+            Assert.IsTrue(taxonomy.EntriesCount == 0);
+            Assert.IsTrue(taxonomy.EntriesLookup.Count == 0);
+        }
+
+        [TestMethod]
+        public void ShouldContainEntry()
+        {
+            RDFOntologyTaxonomy taxonomy = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Model, true);
+            RDFOntologyTaxonomyEntry taxonomyEntry = new RDFOntologyTaxonomyEntry(new RDFOntologyClass(new RDFResource("ex:class")),new RDFOntologyProperty(new RDFResource("ex:predicate")),new RDFOntologyLiteral(new RDFPlainLiteral("lit")));
+            taxonomy.AddEntry(taxonomyEntry);
+            
+            Assert.IsTrue(taxonomy.ContainsEntry(taxonomyEntry));
+        }
+
+        [TestMethod]
+        public void ShouldNotContainEntry()
+        {
+            RDFOntologyTaxonomy taxonomy = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Model, true);
+            RDFOntologyTaxonomyEntry taxonomyEntry = new RDFOntologyTaxonomyEntry(new RDFOntologyClass(new RDFResource("ex:class")),new RDFOntologyProperty(new RDFResource("ex:predicate")),new RDFOntologyLiteral(new RDFPlainLiteral("lit")));
+            RDFOntologyTaxonomyEntry taxonomyEntry2 = new RDFOntologyTaxonomyEntry(new RDFOntologyClass(new RDFResource("ex:class")),new RDFOntologyProperty(new RDFResource("ex:predicate")),new RDFOntologyLiteral(new RDFPlainLiteral("lit2")));
+            taxonomy.AddEntry(taxonomyEntry);
+            
+            Assert.IsFalse(taxonomy.ContainsEntry(taxonomyEntry2));
+            Assert.IsFalse(taxonomy.ContainsEntry(null));
         }
         #endregion
     }
