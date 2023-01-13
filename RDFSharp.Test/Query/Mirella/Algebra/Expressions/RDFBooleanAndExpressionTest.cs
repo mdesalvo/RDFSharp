023ab9d298eb51bac7b414acc65782a650aa174a
Marco De Salvo <marco.desalvo@vodafone.com>
2023-01-13 15:30:52 +0100

    Added boolean expression (and, or)

---
+++ RDFSharp.Test/Query/Mirella/Algebra/Expressions/RDFBooleanAndExpressionTest.cs
@@ -0,0 +1,197 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data;
+using RDFSharp.Model;
+using RDFSharp.Query;
+
+namespace RDFSharp.Test.Query
+{
+    [TestClass]
+    public class RDFBooleanAndExpressionTest
+    {
+        #region Tests
+        [TestMethod]
+        public void ShouldCreateBooleanAndExpression()
+        {
+            RDFBooleanAndExpression expression = new RDFBooleanAndExpression(
+                new RDFComparisonExpression(RDFQueryEnums.RDFComparisonFlavors.EqualTo,
+                    new RDFAddExpression(new RDFVariable("?V1"), new RDFVariable("?V2")),
+                    new RDFConstantExpression(new RDFTypedLiteral("24.08", RDFModelEnums.RDFDatatypes.XSD_FLOAT))),
+                new RDFComparisonExpression(RDFQueryEnums.RDFComparisonFlavors.EqualTo,
+                    new RDFDynamicExpression(new RDFVariable("?V1")),
+                    new RDFConstantExpression(new RDFTypedLiteral("hello", RDFModelEnums.RDFDatatypes.XSD_STRING))));
+
+            Assert.IsNotNull(expression);
+            Assert.IsNotNull(expression.LeftArgument);
+            Assert.IsNotNull(expression.RightArgument);
+            Assert.IsTrue(expression.ToString().Equals("(((?V1 + ?V2) = (24.08)) && ((?V1) = (\"hello\"^^<http://www.w3.org/2001/XMLSchema#string>)))"));
+            Assert.IsTrue(expression.ToString(new List<RDFNamespace>()).Equals("(((?V1 + ?V2) = (24.08)) && ((?V1) = (\"hello\"^^<http://www.w3.org/2001/XMLSchema#string>)))"));
+            Assert.IsTrue(expression.ToString(new List<RDFNamespace>() { RDFNamespaceRegister.GetByPrefix("xsd") }).Equals("(((?V1 + ?V2) = (24.08)) && ((?V1) = (\"hello\"^^xsd:string)))"));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingBooleanAndExpressionBecauseNullLeftArgument()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFBooleanAndExpression(null, new RDFDynamicExpression(new RDFVariable("?V"))));
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingBooleanAndExpressionBecauseNullRightArgument()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFBooleanAndExpression(new RDFDynamicExpression(new RDFVariable("?V")), null));
+
+        [TestMethod]
+        public void ShouldApplyExpressionAndCalculateResultTrue()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFBooleanAndExpression expression = new RDFBooleanAndExpression(
+                new RDFComparisonExpression(RDFQueryEnums.RDFComparisonFlavors.EqualTo,
+                    new RDFAddExpression(new RDFVariable("?A"), new RDFVariable("?B")),
+                    new RDFConstantExpression(new RDFTypedLiteral("30.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE))),
+                new RDFConstantExpression(RDFTypedLiteral.True));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.True));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionAndCalculateResultTrue2()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            table.Columns.Add("?C", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            row["?C"] = RDFTypedLiteral.True.ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFBooleanAndExpression expression = new RDFBooleanAndExpression(
+                new RDFComparisonExpression(RDFQueryEnums.RDFComparisonFlavors.EqualTo,
+                    new RDFAddExpression(new RDFVariable("?A"), new RDFVariable("?B")),
+                    new RDFConstantExpression(new RDFTypedLiteral("30.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE))),
+                new RDFDynamicExpression(new RDFVariable("?C")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.True));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionAndCalculateResultFalse()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFBooleanAndExpression expression = new RDFBooleanAndExpression(
+                new RDFComparisonExpression(RDFQueryEnums.RDFComparisonFlavors.EqualTo,
+                    new RDFAddExpression(new RDFVariable("?A"), new RDFVariable("?B")),
+                    new RDFConstantExpression(new RDFTypedLiteral("30.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE))),
+                new RDFConstantExpression(RDFTypedLiteral.False));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionAndCalculateResultFalse2()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            table.Columns.Add("?C", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            row["?C"] = RDFTypedLiteral.False.ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFBooleanAndExpression expression = new RDFBooleanAndExpression(
+                new RDFComparisonExpression(RDFQueryEnums.RDFComparisonFlavors.EqualTo,
+                    new RDFAddExpression(new RDFVariable("?A"), new RDFVariable("?B")),
+                    new RDFConstantExpression(new RDFTypedLiteral("30.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE))),
+                new RDFDynamicExpression(new RDFVariable("?C")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionAndNotCalculateResultBecauseNotBooleanLeftResponse()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFPlainLiteral("hello").ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFBooleanAndExpression expression = new RDFBooleanAndExpression(
+                new RDFComparisonExpression(RDFQueryEnums.RDFComparisonFlavors.EqualTo,
+                    new RDFAddExpression(new RDFVariable("?A"), new RDFVariable("?B")),
+                    new RDFConstantExpression(new RDFTypedLiteral("35", RDFModelEnums.RDFDatatypes.XSD_DOUBLE))),
+                new RDFConstantExpression(RDFTypedLiteral.True));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionAndNotCalculateResultBecauseNotBooleanRightResponse()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFBooleanAndExpression expression = new RDFBooleanAndExpression(
+                new RDFComparisonExpression(RDFQueryEnums.RDFComparisonFlavors.EqualTo,
+                    new RDFAddExpression(new RDFVariable("?A"), new RDFVariable("?B")),
+                    new RDFConstantExpression(new RDFTypedLiteral("30.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE))),
+                new RDFDynamicExpression(new RDFVariable("?Q")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+        #endregion
+    }
+}
---
+++ RDFSharp/Query/Mirella/Algebra/Expressions/RDFBooleanOrExpression.cs
@@ -0,0 +1,98 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using RDFSharp.Model;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace RDFSharp.Query
+{
+    /// <summary>
+    /// RDFBooleanOrExpression represents a boolean "OR" expression to be applied on a query results table.
+    /// </summary>
+    public class RDFBooleanOrExpression : RDFExpression
+    {
+        #region Ctors
+        /// <summary>
+        /// Default-ctor to build a boolean "OR" expression with given arguments
+        /// </summary>
+        public RDFBooleanOrExpression(RDFExpression leftArgument, RDFExpression rightArgument) 
+            : base(leftArgument, rightArgument)
+        {
+            if (rightArgument == null)
+                throw new RDFQueryException("Cannot create expression because given \"rightArgument\" parameter is null");
+        }
+        #endregion
+
+        #region Interfaces
+        /// <summary>
+        /// Gives the string representation of the boolean "OR" expression
+        /// </summary>
+        public override string ToString()
+            => this.ToString(new List<RDFNamespace>());
+        internal override string ToString(List<RDFNamespace> prefixes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //(L || R)
+            sb.Append('(');
+            sb.Append(((RDFExpression)LeftArgument).ToString(prefixes));
+            sb.Append(" || ");
+            sb.Append(((RDFExpression)RightArgument).ToString(prefixes));
+            sb.Append(')');
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Applies the boolean "OR" expression on the given datarow
+        /// </summary>
+        internal override RDFPatternMember ApplyExpression(DataRow row)
+        {
+            RDFTypedLiteral expressionResult = null;
+
+            try
+            {
+                #region Evaluate Arguments
+                //Evaluate left argument
+                RDFPatternMember leftArgumentPMember = ((RDFExpression)LeftArgument).ApplyExpression(row);
+
+                //Evaluate right argument
+                RDFPatternMember rightArgumentPMember = ((RDFExpression)RightArgument).ApplyExpression(row);
+                #endregion
+
+                #region Calculate Result
+                if (leftArgumentPMember is RDFTypedLiteral leftArgumentTypedLiteral
+                     && leftArgumentTypedLiteral.HasBooleanDatatype()
+                      && rightArgumentPMember is RDFTypedLiteral rightArgumentTypedLiteral
+                       && rightArgumentTypedLiteral.HasBooleanDatatype())
+                {
+                    if (bool.TryParse(leftArgumentTypedLiteral.Value,out bool leftArgumentBooleanValue)
+                          && bool.TryParse(rightArgumentTypedLiteral.Value, out bool rightArgumentBooleanValue))
+                    expressionResult = (leftArgumentBooleanValue || rightArgumentBooleanValue) ? RDFTypedLiteral.True : RDFTypedLiteral.False;
+                }
+                #endregion
+            }
+            catch { /* Just a no-op, since type errors are normal when trying to face variable's bindings */ }
+
+            return expressionResult;
+        }
+        #endregion
+    }
+}
