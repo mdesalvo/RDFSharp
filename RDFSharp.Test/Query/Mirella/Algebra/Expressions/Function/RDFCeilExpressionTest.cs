d04f3b366bacf0da70a3d7e4d6f9d59a8894b8fa
Marco De Salvo <floating_point_9@msn.com>
2023-01-14 12:47:36 +0100

    Added CEIL expression

---
+++ RDFSharp.Test/Query/Mirella/Algebra/Expressions/Function/RDFCeilExpressionTest.cs
@@ -0,0 +1,180 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data;
+using RDFSharp.Model;
+using RDFSharp.Query;
+
+namespace RDFSharp.Test.Query
+{
+    [TestClass]
+    public class RDFCeilExpressionTest
+    {
+        #region Tests
+        [TestMethod]
+        public void ShouldCreateCeilExpressionWithExpression()
+        {
+            RDFCeilExpression expression = new RDFCeilExpression(
+                new RDFAddExpression(new RDFVariable("?V1"), new RDFVariable("?V2")));
+
+            Assert.IsNotNull(expression);
+            Assert.IsNotNull(expression.LeftArgument);
+            Assert.IsNull(expression.RightArgument);
+            Assert.IsTrue(expression.ToString().Equals("(CEIL((?V1 + ?V2)))"));
+            Assert.IsTrue(expression.ToString(new List<RDFNamespace>()).Equals("(CEIL((?V1 + ?V2)))"));
+        }
+
+        [TestMethod]
+        public void ShouldCreateCeilExpressionWithVariable()
+        {
+            RDFCeilExpression expression = new RDFCeilExpression(
+                new RDFVariable("?V1"));
+
+            Assert.IsNotNull(expression);
+            Assert.IsNotNull(expression.LeftArgument);
+            Assert.IsNull(expression.RightArgument);
+            Assert.IsTrue(expression.ToString().Equals("(CEIL(?V1))"));
+            Assert.IsTrue(expression.ToString(new List<RDFNamespace>()).Equals("(CEIL(?V1))"));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingCeilExpressionWithExpressionBecauseNullLeftArgument()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFCeilExpression(null as RDFMathExpression));
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingCeilExpressionWithVariableBecauseNullLeftArgument()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFCeilExpression(null as RDFVariable));
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndCalculateResult()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFCeilExpression expression = new RDFCeilExpression(
+                new RDFAddExpression(new RDFVariable("?A"), new RDFVariable("?B")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFTypedLiteral("31", RDFModelEnums.RDFDatatypes.XSD_DOUBLE)));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndCalculateResult()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFCeilExpression expression = new RDFCeilExpression(
+                new RDFVariable("?A"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFTypedLiteral("6", RDFModelEnums.RDFDatatypes.XSD_DOUBLE)));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndNotCalculateResultBecauseNotNumericLeft()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_STRING).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFCeilExpression expression = new RDFCeilExpression(
+                new RDFAddExpression(new RDFVariable("?A"), new RDFVariable("?B")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndNotCalculateResultBecauseUnboundLeft()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_FLOAT).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFCeilExpression expression = new RDFCeilExpression(
+                new RDFAddExpression(new RDFVariable("?C"), new RDFVariable("?B")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndNotCalculateResultBecauseNotNumericLeft()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_STRING).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFCeilExpression expression = new RDFCeilExpression(
+                new RDFVariable("?A"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndNotCalculateResultBecauseUnboundLeft()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_FLOAT).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFCeilExpression expression = new RDFCeilExpression(
+                new RDFVariable("?C"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+        #endregion
+    }
+}
---
+++ RDFSharp/Query/Mirella/Algebra/Expressions/Function/RDFCeilExpression.cs
@@ -0,0 +1,108 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using RDFSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Text;
+
+namespace RDFSharp.Query
+{
+    /// <summary>
+    /// RDFCeilExpression represents an arithmetical ceil function to be applied on a query results table.
+    /// </summary>
+    public class RDFCeilExpression : RDFExpression
+    {
+        #region Ctors
+        /// <summary>
+        /// Default-ctor to build an arithmetical ceil function with given arguments
+        /// </summary>
+        public RDFCeilExpression(RDFExpression leftArgument) : base(leftArgument, null as RDFExpression) { }
+
+        /// <summary>
+        /// Default-ctor to build an arithmetical ceil function with given arguments
+        /// </summary>
+        public RDFCeilExpression(RDFVariable leftArgument) : base(leftArgument, null as RDFExpression) { }
+        #endregion
+
+        #region Interfaces
+        /// <summary>
+        /// Gives the string representation of the arithmetical ceil function
+        /// </summary>
+        public override string ToString()
+            => this.ToString(new List<RDFNamespace>());
+        internal override string ToString(List<RDFNamespace> prefixes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //(CEIL(L))
+            sb.Append("(CEIL(");
+            if (LeftArgument is RDFExpression expLeftArgument)
+                sb.Append(expLeftArgument.ToString(prefixes));
+            else
+                sb.Append(RDFQueryPrinter.PrintPatternMember((RDFPatternMember)LeftArgument, prefixes));
+            sb.Append("))");
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Applies the arithmetical ceil function on the given datarow
+        /// </summary>
+        internal override RDFPatternMember ApplyExpression(DataRow row)
+        {
+            RDFTypedLiteral expressionResult = null;
+
+            #region Guards
+            if (LeftArgument is RDFVariable && !row.Table.Columns.Contains(LeftArgument.ToString()))
+                return expressionResult;
+            #endregion
+
+            try
+            {
+                #region Evaluate Arguments
+                //Evaluate left argument (Expression VS Variable)
+                RDFPatternMember leftArgumentPMember = null;
+                if (LeftArgument is RDFExpression leftArgumentExpression)
+                    leftArgumentPMember = leftArgumentExpression.ApplyExpression(row);
+                else
+                    leftArgumentPMember = RDFQueryUtilities.ParseRDFPatternMember(row[LeftArgument.ToString()].ToString());
+
+                //Binding error from arguments
+                if (leftArgumentPMember == null)
+                    return expressionResult;
+                #endregion
+
+                #region Calculate Result
+                if (leftArgumentPMember is RDFTypedLiteral leftArgumentTypedLiteral
+                     && leftArgumentTypedLiteral.HasDecimalDatatype())
+                {
+                    if (double.TryParse(leftArgumentTypedLiteral.Value, NumberStyles.Float, CultureInfo.InvariantCulture, out double leftArgumentNumericValue))
+                        expressionResult = new RDFTypedLiteral(Convert.ToString(Math.Ceiling(leftArgumentNumericValue), CultureInfo.InvariantCulture), RDFModelEnums.RDFDatatypes.XSD_DOUBLE);
+                }
+                #endregion
+            }
+            catch { /* Just a no-op, since type errors are normal when trying to face variable's bindings */ }
+
+            return expressionResult;
+        }
+        #endregion
+    }
+}
