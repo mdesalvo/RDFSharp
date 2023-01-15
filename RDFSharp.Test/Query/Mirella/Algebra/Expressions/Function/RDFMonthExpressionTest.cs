6e46a7e5facd730f70fb432f9f9e1819a2684643
Marco De Salvo <floating_point_9@msn.com>
2023-01-15 10:26:52 +0100

    Added MONTH function

---
+++ RDFSharp.Test/Query/Mirella/Algebra/Expressions/Function/RDFMonthExpressionTest.cs
@@ -0,0 +1,168 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data;
+using RDFSharp.Model;
+using RDFSharp.Query;
+
+namespace RDFSharp.Test.Query
+{
+    [TestClass]
+    public class RDFMonthExpressionTest
+    {
+        #region Tests
+        [TestMethod]
+        public void ShouldCreateMonthExpressionWithExpression()
+        {
+            RDFMonthExpression expression = new RDFMonthExpression(
+                new RDFAddExpression(new RDFVariable("?V1"), new RDFVariable("?V2")));
+
+            Assert.IsNotNull(expression);
+            Assert.IsNotNull(expression.LeftArgument);
+            Assert.IsNull(expression.RightArgument);
+            Assert.IsTrue(expression.ToString().Equals("(MONTH((?V1 + ?V2)))"));
+            Assert.IsTrue(expression.ToString(new List<RDFNamespace>()).Equals("(MONTH((?V1 + ?V2)))"));
+        }
+
+        [TestMethod]
+        public void ShouldCreateMonthExpressionWithVariable()
+        {
+            RDFMonthExpression expression = new RDFMonthExpression(
+                new RDFVariable("?V1"));
+
+            Assert.IsNotNull(expression);
+            Assert.IsNotNull(expression.LeftArgument);
+            Assert.IsNull(expression.RightArgument);
+            Assert.IsTrue(expression.ToString().Equals("(MONTH(?V1))"));
+            Assert.IsTrue(expression.ToString(new List<RDFNamespace>()).Equals("(MONTH(?V1))"));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingMonthExpressionWithExpressionBecauseNullLeftArgument()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFMonthExpression(null as RDFExpression));
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingMonthExpressionWithVariableBecauseNullLeftArgument()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFMonthExpression(null as RDFVariable));
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndCalculateResult()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("2022-01-15T10:30:00.000Z", RDFModelEnums.RDFDatatypes.XSD_DATETIME).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFMonthExpression expression = new RDFMonthExpression(
+                new RDFVariableExpression(new RDFVariable("?A")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFTypedLiteral("1", RDFModelEnums.RDFDatatypes.XSD_INTEGER)));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndCalculateResult()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("2022-01-15T10:30:00.000Z", RDFModelEnums.RDFDatatypes.XSD_DATETIME).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFMonthExpression expression = new RDFMonthExpression(
+                new RDFVariable("?A"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFTypedLiteral("1", RDFModelEnums.RDFDatatypes.XSD_INTEGER)));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndNotCalculateResultBecauseNotDateTimeLeft()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_STRING).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFMonthExpression expression = new RDFMonthExpression(
+                new RDFVariableExpression(new RDFVariable("?A")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndNotCalculateResultBecauseUnboundLeft()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("2022-01-15T10:00:00.000Z", RDFModelEnums.RDFDatatypes.XSD_DATETIME).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFMonthExpression expression = new RDFMonthExpression(
+                new RDFVariableExpression(new RDFVariable("?C")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndNotCalculateResultBecauseNotDateTimeLeft()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_STRING).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFMonthExpression expression = new RDFMonthExpression(
+                new RDFVariable("?A"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndNotCalculateResultBecauseUnboundLeft()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("2022-01-15T10:00:00.000Z", RDFModelEnums.RDFDatatypes.XSD_DATETIME).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFMonthExpression expression = new RDFMonthExpression(
+                new RDFVariable("?C"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+        #endregion
+    }
+}
---
+++ RDFSharp/Query/Mirella/Algebra/Expressions/Function/RDFMonthExpression.cs
@@ -0,0 +1,108 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using RDFSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Text;
+
+namespace RDFSharp.Query
+{
+    /// <summary>
+    /// RDFMonthExpression represents a datetime month function to be applied on a query results table.
+    /// </summary>
+    public class RDFMonthExpression : RDFExpression
+    {
+        #region Ctors
+        /// <summary>
+        /// Default-ctor to build a datetime month function with given arguments
+        /// </summary>
+        public RDFMonthExpression(RDFExpression leftArgument) : base(leftArgument, null as RDFExpression) { }
+
+        /// <summary>
+        /// Default-ctor to build a datetime month function with given arguments
+        /// </summary>
+        public RDFMonthExpression(RDFVariable leftArgument) : base(leftArgument, null as RDFExpression) { }
+        #endregion
+
+        #region Interfaces
+        /// <summary>
+        /// Gives the string representation of the datetime year function
+        /// </summary>
+        public override string ToString()
+            => this.ToString(new List<RDFNamespace>());
+        internal override string ToString(List<RDFNamespace> prefixes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //(MONTH(L))
+            sb.Append("(MONTH(");
+            if (LeftArgument is RDFExpression expLeftArgument)
+                sb.Append(expLeftArgument.ToString(prefixes));
+            else
+                sb.Append(RDFQueryPrinter.PrintPatternMember((RDFPatternMember)LeftArgument, prefixes));
+            sb.Append("))");
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Applies the datetime month function on the given datarow
+        /// </summary>
+        internal override RDFPatternMember ApplyExpression(DataRow row)
+        {
+            RDFTypedLiteral expressionResult = null;
+
+            #region Guards
+            if (LeftArgument is RDFVariable && !row.Table.Columns.Contains(LeftArgument.ToString()))
+                return expressionResult;
+            #endregion
+
+            try
+            {
+                #region Evaluate Arguments
+                //Evaluate left argument (Expression VS Variable)
+                RDFPatternMember leftArgumentPMember = null;
+                if (LeftArgument is RDFExpression leftArgumentExpression)
+                    leftArgumentPMember = leftArgumentExpression.ApplyExpression(row);
+                else
+                    leftArgumentPMember = RDFQueryUtilities.ParseRDFPatternMember(row[LeftArgument.ToString()].ToString());
+
+                //Binding error from arguments
+                if (leftArgumentPMember == null)
+                    return expressionResult;
+                #endregion
+
+                #region Calculate Result
+                if (leftArgumentPMember is RDFTypedLiteral leftArgumentTypedLiteral
+                     && leftArgumentTypedLiteral.HasDatetimeDatatype())
+                {
+                    if (DateTime.TryParse(leftArgumentTypedLiteral.Value, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out DateTime leftArgumentDateTimeValue))
+                        expressionResult = new RDFTypedLiteral(leftArgumentDateTimeValue.Month.ToString(), RDFModelEnums.RDFDatatypes.XSD_INTEGER);
+                }
+                #endregion
+            }
+            catch { /* Just a no-op, since type errors are normal when trying to face variable's bindings */ }
+
+            return expressionResult;
+        }
+        #endregion
+    }
+}
