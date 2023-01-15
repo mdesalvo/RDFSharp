5e2fc4b1a69a468ec3f4e90931f1728fc8f750df
Marco De Salvo <floating_point_9@msn.com>
2023-01-15 09:55:51 +0100

    Added NOW expression

---
+++ RDFSharp.Test/Query/Mirella/Algebra/Expressions/Function/RDFNowExpressionTest.cs
@@ -0,0 +1,65 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data;
+using RDFSharp.Model;
+using RDFSharp.Query;
+
+namespace RDFSharp.Test.Query
+{
+    [TestClass]
+    public class RDFNowExpressionTest
+    {
+        #region Tests
+        [TestMethod]
+        public void ShouldCreateNowExpression()
+        {
+            RDFNowExpression expression = new RDFNowExpression();
+
+            Assert.IsNotNull(expression);
+            Assert.IsNull(expression.LeftArgument);
+            Assert.IsNull(expression.RightArgument);
+            Assert.IsTrue(expression.ToString().Equals("(NOW())"));
+            Assert.IsTrue(expression.ToString(new List<RDFNamespace>()).Equals("(NOW())"));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionAndCalculateResult()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("5.1", RDFModelEnums.RDFDatatypes.XSD_DOUBLE).ToString();
+            row["?B"] = new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INT).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFNowExpression expression = new RDFNowExpression();
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult is RDFTypedLiteral tlitExprRes && tlitExprRes.HasDatetimeDatatype());
+
+            RDFPatternMember expressionResult2 = expression.ApplyExpression(table.Rows[0]);
+            Assert.IsTrue(expressionResult2 is RDFTypedLiteral tlitExprRes2 && tlitExprRes2.HasDatetimeDatatype());
+            Assert.IsFalse(expressionResult.Equals(expressionResult2));
+        }
+        #endregion
+    }
+}
---
+++ RDFSharp/Query/Mirella/Algebra/Expressions/Function/RDFNowExpression.cs
@@ -0,0 +1,54 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using RDFSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace RDFSharp.Query
+{
+    /// <summary>
+    /// RDFNowExpression represents a datetime now function (represented in UTC) to be applied on a query results table.
+    /// </summary>
+    public class RDFNowExpression : RDFExpression
+    {
+        #region Ctors
+        /// <summary>
+        /// Default-ctor to build a datetime now function
+        /// </summary>
+        public RDFNowExpression() { }
+        #endregion
+
+        #region Interfaces
+        /// <summary>
+        /// Gives the string representation of the datetime now function
+        /// </summary>
+        public override string ToString()
+            => this.ToString(new List<RDFNamespace>());
+        internal override string ToString(List<RDFNamespace> prefixes)
+            => "(NOW())";
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Applies the now expression on the given datarow
+        /// </summary>
+        internal override RDFPatternMember ApplyExpression(DataRow row)
+            => new RDFTypedLiteral(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"), RDFModelEnums.RDFDatatypes.XSD_DATETIME);
+        #endregion
+    }
+}
