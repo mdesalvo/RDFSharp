2719f134868cf87b2755b75d83e7cd64fbed6813
Marco De Salvo <floating_point_9@msn.com>
2023-01-20 20:40:56 +0100

    Added ISNUMERIC filter

---
+++ RDFSharp/Query/Mirella/Algebra/Filters/RDFIsNumericFilter.cs
@@ -0,0 +1,89 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using RDFSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+namespace RDFSharp.Query
+{
+    /// <summary>
+    /// RDFIsNumericFilter represents a filter for numeric typed literal values of a variable.
+    /// </summary>
+    public class RDFIsNumericFilter : RDFFilter
+    {
+        #region Properties
+        /// <summary>
+        /// Name of the variable to be filtered
+        /// </summary>
+        public string VariableName { get; internal set; }
+        #endregion
+
+        #region Ctors
+        /// <summary>
+        /// Default-ctor to build a filter on the given variable
+        /// </summary>
+        public RDFIsNumericFilter(RDFVariable variable)
+        {
+            if (variable == null)
+                throw new RDFQueryException("Cannot create RDFIsNumericFilter because given \"variable\" parameter is null.");
+
+            this.VariableName = variable.ToString();
+        }
+        #endregion
+
+        #region Interfaces
+        /// <summary>
+        /// Gives the string representation of the filter
+        /// </summary>
+        public override string ToString()
+            => this.ToString(new List<RDFNamespace>());
+        internal override string ToString(List<RDFNamespace> prefixes)
+            => $"FILTER ( ISNUMERIC({this.VariableName}) )";
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Applies the filter on the column corresponding to the variable in the given datarow
+        /// </summary>
+        internal override bool ApplyFilter(DataRow row, bool applyNegation)
+        {
+            bool keepRow = true;
+
+            //Check is performed only if the row contains a column named like the filter's variable
+            if (row.Table.Columns.Contains(this.VariableName))
+            {
+                string variableValue = row[this.VariableName].ToString();
+
+                //Successful match if a numeric typed literal can be created with the variable value
+                RDFPatternMember variableValuePMember = RDFQueryUtilities.ParseRDFPatternMember(variableValue);
+                if (variableValuePMember is RDFResource 
+                      || variableValuePMember is RDFPlainLiteral
+                        || (variableValuePMember is RDFTypedLiteral variableValuePMemberTLit && !variableValuePMemberTLit.HasDecimalDatatype()))
+                    keepRow = false;
+
+                //Apply the eventual negation
+                if (applyNegation)
+                    keepRow = !keepRow;
+            }
+
+            return keepRow;
+        }
+        #endregion
+    }
+}
