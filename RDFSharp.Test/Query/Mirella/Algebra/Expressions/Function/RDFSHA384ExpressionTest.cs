4faeecf193590c7b5cd6ed991305812568795b91
Marco De Salvo <floating_point_9@msn.com>
2023-01-21 11:07:55 +0100

    Added SHA384 function

---
+++ RDFSharp.Test/Query/Mirella/Algebra/Expressions/Function/RDFSHA384ExpressionTest.cs
@@ -0,0 +1,295 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data;
+using RDFSharp.Model;
+using RDFSharp.Query;
+
+namespace RDFSharp.Test.Query
+{
+    [TestClass]
+    public class RDFSHA384ExpressionTest
+    {
+        #region Tests
+        [TestMethod]
+        public void ShouldCreateSHA384ExpressionWithExpression()
+        {
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFAddExpression(new RDFVariable("?V1"), new RDFVariable("?V2")));
+
+            Assert.IsNotNull(expression);
+            Assert.IsNotNull(expression.LeftArgument);
+            Assert.IsNull(expression.RightArgument);
+            Assert.IsTrue(expression.ToString().Equals("(SHA384((?V1 + ?V2)))"));
+            Assert.IsTrue(expression.ToString(new List<RDFNamespace>()).Equals("(SHA384((?V1 + ?V2)))"));
+        }
+
+        [TestMethod]
+        public void ShouldCreateSHA384ExpressionWithVariable()
+        {
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariable("?V1"));
+
+            Assert.IsNotNull(expression);
+            Assert.IsNotNull(expression.LeftArgument);
+            Assert.IsNull(expression.RightArgument);
+            Assert.IsTrue(expression.ToString().Equals("(SHA384(?V1))"));
+            Assert.IsTrue(expression.ToString(new List<RDFNamespace>()).Equals("(SHA384(?V1))"));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingSHA384ExpressionWithExpressionBecauseNullLeftArgument()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFSHA384Expression(null as RDFExpression));
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingSHA384ExpressionWithVariableBecauseNullLeftArgument()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFSHA384Expression(null as RDFVariable));
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndCalculateResultOnNull()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = null;
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariableExpression(new RDFVariable("?A")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndCalculateResultOnPlainLiteral()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFPlainLiteral("hello").ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariableExpression(new RDFVariable("?A")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("59e1748777448c69de6b800d7a33bbfb9ff1b463e44354c3553bcdb9c666fa90125a3c79f90397bdf5f6a13de828684f")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndCalculateResultOnPlainLiteralWithLanguage()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFPlainLiteral("hello","en-US").ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariableExpression(new RDFVariable("?A")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("59e1748777448c69de6b800d7a33bbfb9ff1b463e44354c3553bcdb9c666fa90125a3c79f90397bdf5f6a13de828684f")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndCalculateResultOnTypedLiteral()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("hello", RDFModelEnums.RDFDatatypes.RDFS_LITERAL).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariableExpression(new RDFVariable("?A")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("59e1748777448c69de6b800d7a33bbfb9ff1b463e44354c3553bcdb9c666fa90125a3c79f90397bdf5f6a13de828684f")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndCalculateResultOnResource()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFResource("ex:subj").ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariableExpression(new RDFVariable("?A")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("b567ee9d8d8c097dc36c3d10ef32ba4ab2a7136cec57c7e9dbdeead1c39f4fe64fa8eebd3b427cc664b4de0153e2869d")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndNotCalculateResultBecauseNotBoundVariable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("45", RDFModelEnums.RDFDatatypes.XSD_NORMALIZEDSTRING).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariableExpression(new RDFVariable("?Q")));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithExpressionAndNotCalculateResultBecauseBindingError()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("45", RDFModelEnums.RDFDatatypes.XSD_NORMALIZEDSTRING).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFAddExpression(new RDFVariable("?A"), new RDFConstantExpression(new RDFTypedLiteral("10", RDFModelEnums.RDFDatatypes.XSD_INT))));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndCalculateResultOnNull()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = null;
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariable("?A"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndCalculateResultOnPlainLiteral()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFPlainLiteral("hello").ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariable("?A"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("59e1748777448c69de6b800d7a33bbfb9ff1b463e44354c3553bcdb9c666fa90125a3c79f90397bdf5f6a13de828684f")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndCalculateResultOnPlainLiteralWithLanguage()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFPlainLiteral("hello", "en-US").ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariable("?A"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("59e1748777448c69de6b800d7a33bbfb9ff1b463e44354c3553bcdb9c666fa90125a3c79f90397bdf5f6a13de828684f")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndCalculateResultOnTypedLiteral()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("hello", RDFModelEnums.RDFDatatypes.RDFS_LITERAL).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariable("?A"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("59e1748777448c69de6b800d7a33bbfb9ff1b463e44354c3553bcdb9c666fa90125a3c79f90397bdf5f6a13de828684f")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndCalculateResultOnResource()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFResource("ex:subj").ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariable("?A"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNotNull(expressionResult);
+            Assert.IsTrue(expressionResult.Equals(new RDFPlainLiteral("b567ee9d8d8c097dc36c3d10ef32ba4ab2a7136cec57c7e9dbdeead1c39f4fe64fa8eebd3b427cc664b4de0153e2869d")));
+        }
+
+        [TestMethod]
+        public void ShouldApplyExpressionWithVariableAndNotCalculateResultBecauseNotBoundVariable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = new RDFTypedLiteral("45", RDFModelEnums.RDFDatatypes.XSD_NORMALIZEDSTRING).ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            RDFSHA384Expression expression = new RDFSHA384Expression(
+                new RDFVariable("?Q"));
+            RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+            Assert.IsNull(expressionResult);
+        }
+        #endregion
+    }
+}
