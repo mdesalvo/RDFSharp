38457b29f8b6347c718b9f197bdc2849b2e84b29
Marco De Salvo <floating_point_9@msn.com>
2023-01-22 17:20:09 +0100

    #284 Begin modeling BIND operator

---
+++ RDFSharp.Test/Query/Mirella/Algebra/RDFBindTest.cs
@@ -0,0 +1,62 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using RDFSharp.Model;
+using RDFSharp.Query;
+
+namespace RDFSharp.Test.Query
+{
+    [TestClass]
+    public class RDFBindTest
+    {
+        #region Tests
+        [TestMethod]
+        public void ShouldCreateBind()
+        {
+            RDFBind bind = new RDFBind(new RDFVariableExpression(new RDFVariable("?EXP")), new RDFVariable("?BIND"));
+
+            Assert.IsNotNull(bind);
+            Assert.IsNotNull(bind.Expression);
+            Assert.IsNotNull(bind.Variable);
+            Assert.IsTrue(string.Equals(bind.ToString(), "BIND((?EXP) AS ?BIND)"));
+            Assert.IsTrue(string.Equals(bind.ToString(new List<RDFNamespace>()), "BIND((?EXP) AS ?BIND)"));
+        }
+
+        [TestMethod]
+        public void ShouldCreateBindWithPrefixableUris()
+        {
+            RDFBind bind = new RDFBind(new RDFConstantExpression(new RDFTypedLiteral("hello", RDFModelEnums.RDFDatatypes.XSD_STRING)), new RDFVariable("?BIND"));
+
+            Assert.IsNotNull(bind);
+            Assert.IsNotNull(bind.Expression);
+            Assert.IsNotNull(bind.Variable);
+            Assert.IsTrue(string.Equals(bind.ToString(), $"BIND((\"hello\"^^<{RDFVocabulary.XSD.STRING}>) AS ?BIND)"));
+            Assert.IsTrue(string.Equals(bind.ToString(new List<RDFNamespace>()), $"BIND((\"hello\"^^<{RDFVocabulary.XSD.STRING}>) AS ?BIND)"));
+            Assert.IsTrue(string.Equals(bind.ToString(new List<RDFNamespace>() { RDFNamespaceRegister.GetByPrefix("xsd") }), $"BIND((\"hello\"^^xsd:string) AS ?BIND)"));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingBindBecauseNullExpression()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFBind(null, new RDFVariable("?BIND")));
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingBindBecauseNullVariable()
+            => Assert.ThrowsException<RDFQueryException>(() => new RDFBind(new RDFVariableExpression(new RDFVariable("?EXP")), null));
+        #endregion
+    }
+}
---
+++ RDFSharp/Query/Mirella/Algebra/RDFBind.cs
@@ -0,0 +1,65 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using RDFSharp.Model;
+using System.Collections.Generic;
+
+namespace RDFSharp.Query
+{
+    /// <summary>
+    /// RDFBind represents an operator which binds a new variable to results of an expression.
+    /// </summary>
+    public class RDFBind
+    {
+        #region Properties
+        /// <summary>
+        /// Expression evaluated by the bind operator
+        /// </summary>
+        public RDFExpression Expression { get; internal set; }
+
+        /// <summary>
+        /// Variable emitted by the bind operator
+        /// </summary>
+        public RDFVariable Variable { get; internal set; }
+        #endregion
+
+        #region Ctors
+        /// <summary>
+        /// Default ctor for building a bind operator with given expression and variable
+        /// </summary>
+        public RDFBind(RDFExpression expression, RDFVariable variable)
+        {
+            if (expression == null)
+                throw new RDFQueryException("Cannot create RDFBind because given \"expression\" parameter is null");
+            if (variable == null)
+                throw new RDFQueryException("Cannot create RDFBind because given \"variable\" parameter is null");
+
+            Expression = expression;
+            Variable = variable;
+        }
+        #endregion
+
+        #region Interfaces
+        /// <summary>
+        /// Gives the string representation of the bind operator
+        /// </summary>
+        public override string ToString()
+            => ToString(new List<RDFNamespace>());
+        internal string ToString(List<RDFNamespace> prefixes)
+            => $"BIND({Expression.ToString(prefixes)} AS {Variable})";
+        #endregion
+    }
+}
