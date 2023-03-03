372d811e47fb7b418eb885f6652fbb7dfd931365
Marco De Salvo <marco.desalvo@vodafone.com>
2023-03-03 10:24:57 +0100

    Update copyright headers to 2023

--- RDFSharp/Query/Mirella/Algebra/Operations/RDFOperationResult.cs
+++ RDFSharp/Query/Mirella/Algebra/Operations/RDFOperationResult.cs
@@ -1,5 +1,5 @@
 ﻿/*
-   Copyright 2012-2022 Marco De Salvo
+   Copyright 2012-2023 Marco De Salvo
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
