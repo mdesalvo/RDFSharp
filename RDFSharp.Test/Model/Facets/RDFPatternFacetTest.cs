31fcd88308c63bca2b69f9510911f7a40ac243b5
mdesalvo <floating_point_9@msn.com>
2024-05-26 14:18:03 +0200

    .

---
+++ RDFSharp.Test/Model/Facets/RDFPatternFacetTest.cs
@@ -0,0 +1,69 @@
+﻿/*
+   Copyright 2012-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using RDFSharp.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace RDFSharp.Test.Model
+{
+    [TestClass]
+    public class RDFPatternFacetTest
+    {
+        #region Tests
+        [TestMethod]
+        
+        public void ShouldCreatePatternFacet()
+        {
+            RDFPatternFacet facet = new RDFPatternFacet("^ex");
+
+            Assert.IsNotNull(facet);
+            Assert.IsTrue(facet.Pattern == "^ex");
+            Assert.IsTrue(facet.URI.IsBlank);
+        }
+
+		[TestMethod]
+        
+        public void ShouldValidatePatternFacet()
+        {
+            RDFPatternFacet facet = new RDFPatternFacet("^ex");
+
+            Assert.IsTrue(facet.Validate("example"));
+            Assert.IsFalse(facet.Validate(null));
+			Assert.IsFalse(facet.Validate(string.Empty));
+			Assert.IsFalse(facet.Validate("a"));
+
+			RDFPatternFacet facet0 = new RDFPatternFacet(null);
+			Assert.IsTrue(facet0.Validate("abcdef"));
+            Assert.IsTrue(facet0.Validate(null));
+			Assert.IsTrue(facet0.Validate(string.Empty));
+        }
+
+		[TestMethod]
+        
+        public void ShouldConvertPatternFacetToGraph()
+        {
+            RDFPatternFacet facet = new RDFPatternFacet("^ex");
+			RDFGraph graph = facet.ToRDFGraph();
+
+			Assert.IsNotNull(graph);
+            Assert.IsTrue(graph.TriplesCount == 1);
+			Assert.IsTrue(graph.Single().Predicate.Equals(RDFVocabulary.XSD.PATTERN));
+			Assert.IsTrue(graph.Single().Object.Equals(new RDFTypedLiteral("^ex", RDFDatatypeRegister.GetDatatype(RDFVocabulary.XSD.STRING.ToString()))));
+        }
+		#endregion
+    }
+}
---
+++ RDFSharp/Model/Facets/RDFPatternFacet.cs
@@ -0,0 +1,60 @@
+/*
+   Copyright 2012-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Xml.Schema;
+
+namespace RDFSharp.Model
+{
+    /// <summary>
+    /// RDFPatternFacet represents a constraint requiring the values of a literal to match a pattern
+    /// </summary>
+    public class RDFPatternFacet : RDFFacet
+    {
+        #region Properties
+        /// <summary>
+        /// Pattern required by the facet
+        /// </summary>
+        public string Pattern { get; internal set; }
+        #endregion
+
+        #region Ctors
+        /// <summary>
+        /// Builds a facet requiring the given pattern
+        /// </summary>
+        public RDFPatternFacet(string pattern)
+        	=> Pattern = pattern ?? string.Empty;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Gives a graph representation of the pattern facet
+        /// </summary>
+        public override RDFGraph ToRDFGraph()
+			=> new RDFGraph(new List<RDFTriple>() {
+				new RDFTriple(URI, RDFVocabulary.XSD.PATTERN, new RDFTypedLiteral(Pattern, RDFModelEnums.RDFDatatypes.XSD_STRING)) });
+
+        /// <summary>
+        /// Validates the given literal value against the pattern facet
+        /// </summary>
+        public override bool Validate(string literalValue)
+			=> Regex.IsMatch(literalValue ?? string.Empty, Pattern);
+        #endregion
+    }
+}
