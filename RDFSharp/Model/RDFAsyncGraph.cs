aef217978a34aa9248720fbd01ae8faa31950373
mdesalvo <floating_point_9@msn.com>
2024-05-27 19:34:32 +0200

    Removed useless using

--- RDFSharp/Model/RDFAsyncGraph.cs
+++ RDFSharp/Model/RDFAsyncGraph.cs
@@ -17,7 +17,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Data;
 using System.IO;
 using System.Threading.Tasks;
