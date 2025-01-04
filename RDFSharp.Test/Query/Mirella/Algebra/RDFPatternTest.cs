3bda8c088f415ddbdefbc5aa3c8f3a4ebe09051a
mdesalvo <floating_point_9@msn.com>
2025-01-04 18:16:27 +0100

    Update copyright headers to 2025 + Update test nuggets

--- RDFSharp.Test/Query/Mirella/Algebra/RDFPatternTest.cs
+++ RDFSharp.Test/Query/Mirella/Algebra/RDFPatternTest.cs
@@ -1,5 +1,5 @@
 /*
-   Copyright 2012-2024 Marco De Salvo
+   Copyright 2012-2025 Marco De Salvo
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
