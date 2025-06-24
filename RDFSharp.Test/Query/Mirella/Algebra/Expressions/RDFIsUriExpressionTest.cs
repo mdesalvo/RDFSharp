371f77c427c5627ecba15e798abe6da550538986
Marco De Salvo <marco.desalvo@vodafone.com>
2025-06-24 15:21:57 +0200

    Added RDFIsUriExpression

---
+++ RDFSharp.Test/Query/Mirella/Algebra/Expressions/RDFIsUriExpressionTest.cs
@@ -0,0 +1,276 @@
+﻿/*
+   Copyright 2012-2025 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using RDFSharp.Model;
+using RDFSharp.Query;
+
+namespace RDFSharp.Test.Query;
+
+[TestClass]
+public class RDFIsUriExpressionTest
+{
+    #region Tests
+    [TestMethod]
+    public void ShouldCreateIsUriExpressionWithExpression()
+    {
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFAddExpression(new RDFVariable("?V1"), new RDFVariable("?V2")));
+
+        Assert.IsNotNull(expression);
+        Assert.IsNotNull(expression.LeftArgument);
+        Assert.IsNull(expression.RightArgument);
+        Assert.IsTrue(expression.ToString().Equals("(ISURI((?V1 + ?V2)))"));
+        Assert.IsTrue(expression.ToString([]).Equals("(ISURI((?V1 + ?V2)))"));
+    }
+
+    [TestMethod]
+    public void ShouldCreateIsUriExpressionWithVariable()
+    {
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariable("?V1"));
+
+        Assert.IsNotNull(expression);
+        Assert.IsNotNull(expression.LeftArgument);
+        Assert.IsNull(expression.RightArgument);
+        Assert.IsTrue(expression.ToString().Equals("(ISURI(?V1))"));
+        Assert.IsTrue(expression.ToString([]).Equals("(ISURI(?V1))"));
+    }
+
+    [TestMethod]
+    public void ShouldThrowExceptionOnCreatingIsUriExpressionWithExpressionBecauseNullLeftArgument()
+        => Assert.ThrowsExactly<RDFQueryException>(() => _ = new RDFIsUriExpression(null as RDFExpression));
+
+    [TestMethod]
+    public void ShouldThrowExceptionOnCreatingIsUriExpressionWithVariableBecauseNullLeftArgument()
+        => Assert.ThrowsExactly<RDFQueryException>(() => _ = new RDFIsUriExpression(null as RDFVariable));
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithExpressionAndCalculateResultOnNull()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = null;
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariableExpression(new RDFVariable("?A")));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithExpressionAndCalculateResultOnPlainLiteral()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFPlainLiteral("hello").ToString();
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariableExpression(new RDFVariable("?A")));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithExpressionAndCalculateResultOnPlainLiteralWithLanguage()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFPlainLiteral("hello","en-US").ToString();
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariableExpression(new RDFVariable("?A")));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithExpressionAndCalculateResultOnTypedLiteral()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFTypedLiteral("hello", RDFModelEnums.RDFDatatypes.RDFS_LITERAL).ToString();
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariableExpression(new RDFVariable("?A")));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithExpressionAndCalculateResultOnResource()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFResource("ex:subj").ToString();
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariableExpression(new RDFVariable("?A")));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.True));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithExpressionAndNotCalculateResultBecauseNotBoundVariable()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFResource("ex:subj");
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariableExpression(new RDFVariable("?Q")));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNull(expressionResult);
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithVariableAndCalculateResultOnNull()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = null;
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariable("?A"));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithVariableAndCalculateResultOnPlainLiteral()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFPlainLiteral("hello").ToString();
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariable("?A"));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithVariableAndCalculateResultOnPlainLiteralWithLanguage()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFPlainLiteral("hello", "en-US").ToString();
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariable("?A"));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithVariableAndCalculateResultOnTypedLiteral()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFTypedLiteral("hello", RDFModelEnums.RDFDatatypes.RDFS_LITERAL).ToString();
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariable("?A"));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.False));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithVariableAndCalculateResultOnResource()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFResource("ex:subj").ToString();
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariable("?A"));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNotNull(expressionResult);
+        Assert.IsTrue(expressionResult.Equals(RDFTypedLiteral.True));
+    }
+
+    [TestMethod]
+    public void ShouldApplyExpressionWithVariableAndNotCalculateResultBecauseNotBoundVariable()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("?A", typeof(string));
+        DataRow row = table.NewRow();
+        row["?A"] = new RDFTypedLiteral("45", RDFModelEnums.RDFDatatypes.XSD_NORMALIZEDSTRING).ToString();
+        table.Rows.Add(row);
+        table.AcceptChanges();
+
+        RDFIsUriExpression expression = new RDFIsUriExpression(
+            new RDFVariable("?Q"));
+        RDFPatternMember expressionResult = expression.ApplyExpression(table.Rows[0]);
+
+        Assert.IsNull(expressionResult);
+    }
+    #endregion
+}
---
+++ RDFSharp/Query/Mirella/Algebra/Expressions/RDFIsUriExpression.cs
@@ -0,0 +1,99 @@
+﻿/*
+   Copyright 2012-2025 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using RDFSharp.Model;
+
+namespace RDFSharp.Query
+{
+    /// <summary>
+    /// RDFIsUriExpression represents a Uri-checking function to be applied on a query results table.
+    /// </summary>
+    public sealed class RDFIsUriExpression : RDFExpression
+    {
+        #region Ctors
+        /// <summary>
+        /// Default-ctor to build a Uri-checking function with given arguments
+        /// </summary>
+        public RDFIsUriExpression(RDFExpression leftArgument) : base(leftArgument, null as RDFExpression) { }
+
+        /// <summary>
+        /// Default-ctor to build a Uri-checking function with given arguments
+        /// </summary>
+        public RDFIsUriExpression(RDFVariable leftArgument) : base(leftArgument, null as RDFExpression) { }
+        #endregion
+
+        #region Interfaces
+        /// <summary>
+        /// Gives the string representation of the Uri-checking function
+        /// </summary>
+        public override string ToString()
+            => ToString(new List<RDFNamespace>());
+        internal override string ToString(List<RDFNamespace> prefixes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //(ISURI(L))
+            sb.Append("(ISURI(");
+            if (LeftArgument is RDFExpression expLeftArgument)
+                sb.Append(expLeftArgument.ToString(prefixes));
+            else
+                sb.Append(RDFQueryPrinter.PrintPatternMember((RDFPatternMember)LeftArgument, prefixes));
+            sb.Append("))");
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Applies the Uri-checking function on the given datarow
+        /// </summary>
+        internal override RDFPatternMember ApplyExpression(DataRow row)
+        {
+            RDFTypedLiteral expressionResult = null;
+
+            #region Guards
+            if (LeftArgument is RDFVariable && !row.Table.Columns.Contains(LeftArgument.ToString()))
+                return null;
+            #endregion
+
+            try
+            {
+                #region Evaluate Arguments
+                //Evaluate left argument (Expression VS Variable)
+                RDFPatternMember leftArgumentPMember;
+                if (LeftArgument is RDFExpression leftArgumentExpression)
+                    leftArgumentPMember = leftArgumentExpression.ApplyExpression(row);
+                else
+                    leftArgumentPMember = RDFQueryUtilities.ParseRDFPatternMember(row[LeftArgument.ToString()].ToString());
+                #endregion
+
+                #region Calculate Result
+                expressionResult = Uri.TryCreate(leftArgumentPMember.ToString(), UriKind.Absolute, out _)
+                                    ? RDFTypedLiteral.True : RDFTypedLiteral.False;
+                #endregion
+            }
+            catch { /* Just a no-op, since type errors are normal when trying to face variable's bindings */ }
+
+            return expressionResult;
+        }
+        #endregion
+    }
+}
